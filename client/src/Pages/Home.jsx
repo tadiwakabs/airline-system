35026a52f6758200f4c8efe3fe700c25fbb07f1a
Tadiwa Kabayadondo <tadiwakabayadondo@gmail.com>
2026-03-07 13:50:04 -0600

    Initial client setup

---
+++ client/src/Pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home () {
+    return (
+        <>
+            <p className="text-2xl text-red-400">Airline Ticketing System</p>
+            <p>COSC 3380 Team 13</p>
+        </>
+    );
+}
