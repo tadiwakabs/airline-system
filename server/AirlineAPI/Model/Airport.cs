3eae96fb086885f687363fde734aa1affceb3e4e
Shubham Patel <159396731+5hubhamP@users.noreply.github.com>
2026-03-18 19:23:03 -0500

    Updated aiport.cs

--- server/AirlineAPI/Model/Airport.cs
+++ server/AirlineAPI/Model/Airport.cs
@@ -8,5 +8,28 @@ namespace AirlineAPI.Models
     {
         [Key,Required,StringLength(3)]
         public string airportCode{get;set;}=string.Empty;
+        
+        [Required,StringLength(100)]
+        public string airportName{get;set;}= string.Empty;
+
+        [Required,StringLength(30)]
+        public string city{get;set;}=string.Empty;
+
+        [StringLength(2)]
+        public  string? state{get;set;}
+
+        [Required, StringLength(2)]
+        public string country{get;set;}=string.Empty;
+
+        [StringLength(50)]
+        public string? timezone{get;set;}
+
+        //------links-----
+        [ForeignKey("state")]
+        public States? States{get;set;}
+
+        [ForeignKey("country")]
+        public Countries? Countries{get;set;}
+
     }
 }
