99cc61897e71b81fb00af5869210dbf2babbab51
tadiwakabs <tadiwakabayadondo@gmail.com>
2026-04-19 23:04:05 -0500

    Final DB Schema

--- db-queries/Tables/notificationTable.sql
+++ db-queries/Tables/notificationTable.sql
@@ -2,9 +2,9 @@ CREATE TABLE `Notification` (
     `notificationId` int NOT NULL AUTO_INCREMENT,
     `userId` varchar(50) NOT NULL,
     `bookingId` varchar(50) DEFAULT NULL,
-    `flightNum` int NOT NULL,
+    `flightNum` int DEFAULT NULL,
     `message` varchar(255) NOT NULL,
     `createdAt` datetime NOT NULL,
     `notificationStatus` varchar(20) NOT NULL DEFAULT 'Unread',
     PRIMARY KEY (`notificationId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB AUTO_INCREMENT=940 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
